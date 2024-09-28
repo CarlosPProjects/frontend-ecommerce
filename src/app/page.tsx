ce810bbe33088272d52b7dff2849eaf6db65a51f
CarlosPProjects <cg9822@gmail.com>
2024-09-29 00:08:59 +0200

    "Refactor Home page layout and styles"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="overflow-hidden relative flex items-center max-w-[1740px] mx-auto bg-background-light min-h-[850px] rounded-bl-[48px] rounded-br-[48px] ">
+      <main className="overflow-hidden flex items-center max-w-[1740px] mx-auto bg-background-light lg:min-h-[850px] rounded-bl-[48px] rounded-br-[48px] py-8">
         {/* Hero Section */}
         <MaxWidthContainer className="z-10">
-          <div className="flex justify-between items-center gap-14">
-            <div className="flex flex-col gap-2 max-w-[632px]">
+          <div className="flex flex-col lg:flex-row justify-between items-center gap-14">
+            <div className="flex flex-col gap-2 max-w-[632px] mt-16">
               <span className="uppercase text-sm tracking-wider">
                 Welcome to chairy
               </span>
-              <h1 className="font-bold text-[68px] leading-tight capitalize">
+              <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-[68px] leading-tight capitalize">
                 Best Furniture Collection for your interior.
               </h1>
               <Button className="w-fit group mt-4">
@@ -25,17 +25,17 @@ export default function Home() {
                 <MoveRightIcon className="w-6 h-6 group-hover:translate-x-1 transition" />
               </Button>
             </div>
-            <div>
+            <div className="relative">
               <Image
                 src="/assets/featured-hero-product.png"
                 alt="featured wooden chair"
                 width={475}
                 height={649}
               />
+              <div className="absolute w-[750px] h-[750px] bg-[#E1E3E5] rounded-full -z-10 inset-1/2 lg:-inset-1/4 lg:-translate-y-64 place-self-center lg:place-self-end"></div>
             </div>
           </div>
         </MaxWidthContainer>
-        <div className="absolute w-[750px] h-[750px] bg-[#E1E3E5] rounded-full z-0 -top-1/4 right-[5%]"></div>
       </main>
     </>
   );
