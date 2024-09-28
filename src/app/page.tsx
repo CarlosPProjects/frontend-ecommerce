0f486414bbbeb2c8ed1c2acff4eb729074a7136a
CarlosPProjects <cg9822@gmail.com>
2024-09-28 23:34:33 +0200

    "Added @radix-ui/react-slot dependency and updated Home page component with new UI elements and layout"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
 import MaxWidthContainer from "@/components/MaxWidthContainer";
 import FeatureProduct from "@/components/product/featured-product";
+import { Button } from "@/components/ui/button";
+import { ArrowBigRight, MoveRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <MaxWidthContainer>
-      <div>
-        <p>
-          Hello World
-        </p>
-        <FeatureProduct/>
-      </div>
-    </MaxWidthContainer>
+    <>
+      <main className="flex items-center max-w-[1740px] mx-auto bg-background-light min-h-[850px] rounded-bl-[48px] rounded-br-[48px]">
+        <MaxWidthContainer>
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col gap-2 max-w-[632px]">
+              <span className="uppercase text-sm tracking-wider">
+                Welcome to chairy
+              </span>
+              <h1 className="font-bold text-[68px] leading-tight capitalize">
+                Best Furniture Collection for your interior.
+              </h1>
+              <Button className="w-fit group mt-4">
+                <span className="text-base font-semibold leading-tight mr-5 ">
+                  Shop Now
+                </span>
+                <MoveRightIcon className="w-6 h-6 group-hover:translate-x-1 transition" />
+              </Button>
+            </div>
+          </div>
+        </MaxWidthContainer>
+      </main>
+    </>
   );
 }
