4cf700dd87ba6590a4a7ab8325407f983271aacd
CarlosPProjects <cg9822@gmail.com>
2024-09-28 23:48:15 +0200

    "Added Image component to hero section, updated main container styles, and added absolute positioned div element"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import MaxWidthContainer from "@/components/MaxWidthContainer";
 import FeatureProduct from "@/components/product/featured-product";
 import { Button } from "@/components/ui/button";
 import { ArrowBigRight, MoveRightIcon } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <main className="flex items-center max-w-[1740px] mx-auto bg-background-light min-h-[850px] rounded-bl-[48px] rounded-br-[48px]">
-        <MaxWidthContainer>
-          <div className="flex justify-between items-center">
+      <main className="overflow-hidden relative flex items-center max-w-[1740px] mx-auto bg-background-light min-h-[850px] rounded-bl-[48px] rounded-br-[48px] ">
+        {/* Hero Section */}
+        <MaxWidthContainer className="z-10">
+          <div className="flex justify-between items-center gap-14">
             <div className="flex flex-col gap-2 max-w-[632px]">
               <span className="uppercase text-sm tracking-wider">
                 Welcome to chairy
@@ -23,8 +25,17 @@ export default function Home() {
                 <MoveRightIcon className="w-6 h-6 group-hover:translate-x-1 transition" />
               </Button>
             </div>
+            <div>
+              <Image
+                src="/assets/featured-hero-product.png"
+                alt="featured wooden chair"
+                width={475}
+                height={649}
+              />
+            </div>
           </div>
         </MaxWidthContainer>
+        <div className="absolute w-[750px] h-[750px] bg-[#E1E3E5] rounded-full z-0 -top-1/4 right-[5%]"></div>
       </main>
     </>
   );
