094a814de811975985ad421ecdf63a0b8bdf809e
CarlosPProjects <cg9822@gmail.com>
2024-10-04 20:13:49 +0200

    Created cart page

---
+++ src/app/(routes)/cart/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthContainer from '@/components/MaxWidthContainer'
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <MaxWidthContainer>
+      <div>
+        Cart
+      </div>
+    </MaxWidthContainer>
+  )
+}
+
+export default Cart
