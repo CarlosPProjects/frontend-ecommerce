d8bd904a7bf7929c84831f02b6c4dabf36b82906
CarlosPProjects <cg9822@gmail.com>
2024-10-04 19:01:20 +0200

    "Updated product page layout and added cart button"

--- src/app/(routes)/product/[productSlug]/page.tsx
+++ src/app/(routes)/product/[productSlug]/page.tsx
@@ -2,9 +2,12 @@
 
 import useGetSingleProduct from "@/api/useGetSingleProduct";
 import MaxWidthContainer from "@/components/MaxWidthContainer";
+import { Button } from "@/components/ui/button";
 import { Product } from "@/types/product";
 import { TResponse } from "@/types/response";
+import { MoveRightIcon, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 const Page = () => {
@@ -26,25 +29,43 @@ const Page = () => {
     const { name, description, images, price, salePrice }: Product = result[0];
 
     return (
-      <MaxWidthContainer className="my-20">
-        <div className="flex flex-wrap justify-between gap-8">
-          <div className="">
-            <div className="w-[500px] h-[500px] relative bg-no-repeat bg-cover bg-center">
-              <Image
-                src={url + images[0].formats.medium.url}
-                alt={name}
-                fill
-                className="object-cover rounded-xl"
-              />
+      <>
+        <MaxWidthContainer className="bg-background-light">
+          <div className="py-4  ">
+            <Link href="/" className="font-semibold">
+              Inicio
+            </Link>{" "}
+            / {name}
+          </div>
+        </MaxWidthContainer>
+        <MaxWidthContainer className="my-20">
+          <div className="flex flex-wrap justify-between gap-8">
+            <div className="">
+              <div className="w-[500px] h-[500px] relative bg-no-repeat bg-cover bg-center">
+                <Image
+                  src={url + images[0].formats.medium.url}
+                  alt={name}
+                  fill
+                  className="object-cover rounded-xl"
+                />
+              </div>
+            </div>
+            <div className="w-full max-w-[500px] space-y-4">
+              <h1 className="text-[28px] sm:text-[32px] font-semibold leading-tight">
+                {name}
+              </h1>
+              <p>{description}</p>
+              <p className="text-xl md:text-2xl font-bold pb-4">${price} </p>
+              <Button>
+                <span className="text-base font-semibold leading-tight mr-5 ">
+                  Add to cart
+                </span>
+                <ShoppingCart className="w-6 h-6 group-hover:translate-x-1 transition" />
+              </Button>
             </div>
           </div>
-          <div className="w-full max-w-[500px]">
-            <h1 className="text-[28px] sm:text-[32px] font-semibold leading-tight">
-              {name}
-            </h1>
-          </div>
-        </div>
-      </MaxWidthContainer>
+        </MaxWidthContainer>
+      </>
     );
   }
 };
